05047f7cf64afe81c45b80b8db33cc0e7d0c1bd7
JiUnKang <46744270+rkdwldns00@users.noreply.github.com>
2022-08-25 11:29:25 +0900

    last commit
    오프라인버전 완성

--- Zombie/Assets/Scripts/Zombie.cs
+++ Zombie/Assets/Scripts/Zombie.cs
@@ -42,7 +42,7 @@ public class Zombie : LivingEntity
         navMeshAgent = GetComponent<NavMeshAgent>();
         zombieAnimator = GetComponent<Animator>();
         zombieAudioPlayer = GetComponent<AudioSource>();
-        zombieRenderer = GetComponent<Renderer>();
+        zombieRenderer = GetComponentInChildren<Renderer>();
     }
 
     // 좀비 AI의 초기 스펙을 결정하는 셋업 메서드
